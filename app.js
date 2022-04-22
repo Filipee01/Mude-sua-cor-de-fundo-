b46207d9dd788f014f9822a178dd6b062ef411f4
Filipee01 <99333746+Filipee01@users.noreply.github.com>
2022-04-21 23:35:26 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+document.getElementById('btn-amarelo').addEventListener('click', function () {
+  document.querySelector('body').setAttribute('class', 'amarelo')
+})
+
+document.getElementById('btn-vermelho').addEventListener('click', function () {
+  document.querySelector('body').setAttribute('class', 'vermelho')
+})
+
+document.getElementById('btn-roxo').addEventListener('click', function () {
+  document.querySelector('body').setAttribute('class', 'roxo')
+})
+
+document.getElementById('btn-rosa').addEventListener('click', function () {
+  document.querySelector('body').setAttribute('class', 'rosa')
+})
+
+document.getElementById('btn-azul').addEventListener('click', function () {
+  document.querySelector('body').setAttribute('class', 'azul')
+})
+
+document.getElementById('btn-verde').addEventListener('click', function () {
+  document.querySelector('body').setAttribute('class', 'verde')
+})
